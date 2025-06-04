4c79e634955e6ff0fb9abcf00a791ca61e30ace1
Stephanie May <3074691+mizmay@users.noreply.github.com>
2025-06-04 16:21:06 -0700

    Try some pre-colored sprites

--- templates/layers/muddy_branch_access.js
+++ templates/layers/muddy_branch_access.js
@@ -1,14 +1,19 @@
 module.exports.default = (context) => {
   const baseStyle = {
     "id": "muddy_branch_access",
-    "type": "circle",
+    "type": "symbol",
     "source": "muddy_branch_relation",
     "filter": ["==", "$type", "Point"],
+    "layout": {
+      "icon-image": "car_11",
+      "icon-size": ["interpolate", ["linear"], ["zoom"], 13, 1, 16, 1.5],
+      "icon-allow-overlap": true,
+      "icon-ignore-placement": true
+    },
     "paint": {
-      "circle-color": ["case",["==",["get", "ford"], "yes"], context.colors.water_dark, context.colors.trail_highzoom],
-      "circle-radius": ["case",["==",["get", "highway"], "trailhead"], 4.5, 3.5],
-      "circle-stroke-width": 1,
-      "circle-stroke-color": ["case",["==",["get", "ford"], "yes"], context.colors.water_light, context.colors.halo],
+      "icon-color": ["case",["==",["get", "ford"], "yes"], context.colors.water_dark, context.colors.trail_highzoom],
+      "icon-halo-color": ["case",["==",["get", "ford"], "yes"], context.colors.water_light, context.colors.halo],
+      "icon-halo-width": 1,
     }
   };
   let overrides = {};
