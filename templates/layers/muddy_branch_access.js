60727f583dd4aa66bdb646bef083c1c67523b0bd
Stephanie May <3074691+mizmay@users.noreply.github.com>
2025-06-05 09:27:20 -0700

    Style trailhead labels

--- templates/layers/muddy_branch_access.js
+++ templates/layers/muddy_branch_access.js
@@ -6,19 +6,22 @@ module.exports.default = (context) => {
     "filter": ["==", ["get", "highway"], "trailhead"],
     "layout": {
       "icon-image": "trailhead",
-      "icon-size": ["interpolate", ["linear"], ["zoom"], 13, 0.8, 16, 1.2, 18, 2],
+      "icon-size": ["interpolate", ["exponential", 1], ["zoom"], 13, 0.8, 16, 1, 18, 1.2],
       "icon-allow-overlap": true,
-      "text-field": "Trailhead",
+      "text-field": ["step", ["zoom"], "", 15, "Trailhead"],
       "text-font": ["Noto Sans Regular"],
-      "text-size": ["interpolate", ["exponential", 0.7], ["zoom"], 13, 12, 18, 14],
+      "text-size": ["interpolate", ["exponential", 0.7], ["zoom"], 13, 11, 18, 12],
       "text-justify": "auto",
-      "text-variable-anchor": ["left", "right", "bottom", "top"],
-      "text-variable-anchor-offset": ["interpolate", ["linear"], ["zoom"],
-        13, ["literal",["left", [0.5, 0], "right", [-0.5, 0], "bottom", [0, 0.5], "top", [0, 0.5]]],
-        16, ["literal",["left", [1, 0], "right", [-1, 0], "bottom", [0, 1], "top", [0, 1]]],
-        18, ["literal",["left", [1.2, 0], "right", [-1.2, 0], "bottom", [0, 1.2], "top", [0, 1.2]]]
+      "text-variable-anchor": ["left", "right", "bottom"],
+      "text-variable-anchor-offset": ["interpolate", ["exponential", 1.7], ["zoom"],
+        12, ["literal",["left", [0.6, 0], "right", [-0.6, 0], "bottom", [0, 0.6]]],
+        20, ["literal",["left", [1.2, 0], "right", [-1.2, 0], "bottom", [0, 1.2]]]
       ],
       "text-padding": 2
+    }, "paint": {
+      "text-color": context.colors.trailLabel,
+      "text-halo-color": context.colors.other_vegetation,
+      "text-halo-width": 1,
     }
   };
   let overrides = {};
