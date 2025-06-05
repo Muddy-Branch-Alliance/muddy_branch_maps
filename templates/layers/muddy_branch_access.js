cc80a9c07417aeb526e36c0625431abf0e4fabdc
Stephanie May <3074691+mizmay@users.noreply.github.com>
2025-06-04 18:41:37 -0700

    Update trailhead labels

--- templates/layers/muddy_branch_access.js
+++ templates/layers/muddy_branch_access.js
@@ -5,10 +5,20 @@ module.exports.default = (context) => {
     "source": "muddy_branch_relation",
     "filter": ["==", ["get", "highway"], "trailhead"],
     "layout": {
-      "icon-image": "trail_compass",
-      "icon-size": ["interpolate", ["linear"], ["zoom"], 13, 0.4, 16, 0.6, 18, 1.5],
+      "icon-image": "trailhead",
+      "icon-size": ["interpolate", ["linear"], ["zoom"], 13, 0.8, 16, 1.2, 18, 2],
       "icon-allow-overlap": true,
-      "icon-ignore-placement": true
+      "text-field": "Trailhead",
+      "text-font": ["Noto Sans Regular"],
+      "text-size": ["interpolate", ["exponential", 0.7], ["zoom"], 13, 12, 18, 14],
+      "text-justify": "auto",
+      "text-variable-anchor": ["left", "right", "bottom", "top"],
+      "text-variable-anchor-offset": ["interpolate", ["linear"], ["zoom"],
+        13, ["literal",["left", [0.5, 0], "right", [-0.5, 0], "bottom", [0, 0.5], "top", [0, 0.5]]],
+        16, ["literal",["left", [1, 0], "right", [-1, 0], "bottom", [0, 1], "top", [0, 1]]],
+        18, ["literal",["left", [1.2, 0], "right", [-1.2, 0], "bottom", [0, 1.2], "top", [0, 1.2]]]
+      ],
+      "text-padding": 2
     }
   };
   let overrides = {};
