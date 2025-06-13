e2aa11cac1e0d58fa4a75caabae26313a78ea9e9
Stephanie May <3074691+mizmay@users.noreply.github.com>
2025-06-13 14:54:45 -0700

    Back out trail width a tiny bit

--- templates/layers/muddy_branch_trail.js
+++ templates/layers/muddy_branch_trail.js
@@ -7,7 +7,7 @@ module.exports.default = (context) => {
     "paint": {
       "line-color": ["interpolate", ["exponential", 2.5], ["zoom"], 11, context.colors.trail_lowzoom, 18, context.colors.trail_highzoom],
       "line-dasharray": ["step",["zoom"], ["literal", [3, 1]], 16, ["literal", [2, 1]], 18, ["literal", [2, 0]] ],
-      "line-width": ["interpolate", ["exponential", 1.5], ["zoom"], 11, 2, 20, 12]
+      "line-width": ["interpolate", ["exponential", 1.5], ["zoom"], 11, 1.5, 20, 12]
     }
   };
   let overrides = {};
